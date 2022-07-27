e1bb3f695418e38bd1081d4346e552376df0967d
stenio-fonteles <macacovelho.ss@gmail.com>
2022-07-27 18:13:28 -0300

    adesao da foto do pokemon quando selecionado

--- src/pokedex/index.tsx
+++ src/pokedex/index.tsx
@@ -5,36 +5,46 @@ import SelectPokemon from "./components";
 export default function Pokedex(){
     const [dataPokemon, setDataPokemon] = useState([])
     const [paginations, setPaginations] =useState([])
+    const [bioPokemon, setBioPokemon] = useState([])
 
     async function getPokemon(){
         const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon");
-        console.log(data)
-        setDataPokemon(data.results);
-       setPaginations(data);
-    }
-
-    function selectPokemon(id) {
-        //console.log(dataPokemon)
-    }
-    async function handleNext(){
-        const {data} = await axios.get(paginations.next);
         setDataPokemon(data.results);
         setPaginations(data);
     }
 
+    async function getBioPokemon(indice) {
+        let newIndice = indice + 1
+        const {data} =  await axios.get(`http://pokeapi.co/api/v2/pokemon-form/${newIndice}/`)
+        setBioPokemon(data.sprites)
+    }
     
+    console.log(bioPokemon)
+
+
+     function selectPokemon(id) {
+        let indice = dataPokemon.findIndex(key => key == id)
+        getBioPokemon(indice)
+    }
+    async function handleNext(){
+        const {data} = await axios.get(paginations.next);
+        setDataPokemon(data.results);
+        setPaginations(data.sprites);
+    }
+    
+
     useEffect(() => { getPokemon()},[])
     return(
         <>
         {dataPokemon.map((pokemon) =>{
             return(
-                <button onClick={ () => selectPokemon(pokemon)}key={pokemon.name}>{pokemon.name}</button>
+                <button onClick={ () => selectPokemon(pokemon) }key={pokemon.name}>{pokemon.name}</button>
             )
         })}
-        <button onClick={handleNext}></button>
+        {<br></br>}
+        <button onClick={handleNext}>- Próximo -</button>
+        <img src={bioPokemon.back_default}/>
 
-        
-        
         </>
     )
 }
